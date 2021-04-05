c5a87f88a2ea00beae8ef29af75411ca9ed07fc0
Gustaaf Wehnes <>
2021-04-05 04:22:00 +0000

    clean up bash file

--- run.bash
+++ run.bash
@@ -5,7 +5,7 @@ echo "run Test4.java"
 java -classpath .:/run_dir/junit-4.12.jar:/run_dir/hamcrest-core-1.3.jar:/run_dir/json-simple-1.1.1.jar:./bin ManualTest4
 
 echo "run Test5.java"
-java -jar "./Jars/junit-platform-console-standalone-1.5.0.jar" --disable-banner --disable-ansi-colors --details-theme=ascii --details=tree -class-path ./bin -c GameOfLifeTest
+#java -jar "./Jars/junit-platform-console-standalone-1.5.0.jar" --disable-banner --disable-ansi-colors --details-theme=ascii --details=tree -class-path ./bin -c GameOfLifeTest
 
 echo "run Test5.java"
 java -jar "./Jars/junit-platform-console-standalone-1.5.0.jar" --disable-banner --disable-ansi-colors --details-theme=ascii --details=tree -class-path ./bin -c TriangleClassifierTest5
